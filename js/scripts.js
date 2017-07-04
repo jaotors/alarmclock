b46bac8a3bb1c526a6e9aea719b7347203a62fed
jaotors <turingan.joshua@gmail.com>
2017-07-04 18:31:25 +0800

    added alarm in log

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
   //set clock
   let date = new Date()
-  let hours = date.getHours(), minutes = date.getMinutes(), seconds = date.getSeconds(), ampm = ''
+  let hours = date.getHours(), minutes = date.getMinutes(), seconds = date.getSeconds(), ampm = '', mHours = 0
   let setHours = document.getElementsByClassName('hh')[0]
   let setMinutes = document.getElementsByClassName('mm')[0]
   let setSeconds = document.getElementsByClassName('ss')[0]
@@ -13,17 +13,18 @@
   let inputAmPm = document.getElementById('inputAmPm')
   let inputNote = document.getElementById('inputNote')
   let ulListAlarm = document.getElementsByClassName('list-alarm')[0]
+  let alarmInputs = '';
 
   let checkZero = (number) => {
     if(number < 10) {
-      number = '0' + number
+      number = ('0' + number).slice(-2)
     }
 
     return number 
   }
 
   let udpateSixty = (number) => {
-    if (number > 59) {
+    if(number > 59) {
       number = 0
     }
 
@@ -33,7 +34,7 @@
   let updateSeconds = () => {
     seconds = udpateSixty(seconds)
 
-    if (seconds == 0) {
+    if(seconds == 0) {
       updateMinutes(minutes)
     }
 
@@ -46,7 +47,7 @@
     min = udpateSixty(min)
     min = checkZero(min)
 
-    if (min == 0) {
+    if(min == 0) {
       hours++
       updateHours(hours)
     }
@@ -57,9 +58,9 @@
 
   let checkAmPm = (hour) => {
     let last = ''
-    if (hour < 12) {
+    if(hour < 12) {
       last = 'AM'
-    } else if (hour >= 12) {
+    } else if(hour >= 12) {
       last = 'PM'
     }
 
@@ -72,9 +73,9 @@
     }
 
     hr = ((parseInt(hr) + 11) % 12) + 1
-    console.log(hours + '  ' + hr)
     ampm = checkAmPm(hours)
     hr = checkZero(hr)
+    mHours = hr;
     
     setHours.innerHTML = hr
     setAmPm.innerHTML = ampm
@@ -82,7 +83,7 @@
 
   let validateInput = (e) => {
     value = e.target.value
-    if (value.length > 1) {
+    if(value.length > 1) {
       e.preventDefault()
     }
   }
@@ -92,7 +93,7 @@
   let addAlarm = () => {
     let alarm = Object.create(alarmObj)
     
-    let time = checkZero(inputHours.value) + ':' + inputMinutes.value + inputAmPm.value.toUpperCase()
+    let time = checkZero(inputHours.value) + ':' + checkZero(inputMinutes.value) + inputAmPm.value.toUpperCase()
     alarm.time = time
     alarm.note = inputNote.value
 
@@ -102,14 +103,43 @@
 
   // list an alarm
   let listAlarm = () => {
-
+    ulListAlarm.innerHTML = '';
     alarmList.map((time, index) => {
       let li = document.createElement('li')
-      li.innerHTML = '<input type="checkbox" name="alarms" value=' + time.time + '> <span>' + time.time + '</span> ' + time.note
+      li.innerHTML = '<input type="checkbox" class="alarmInputs" name="alarms" value=' + time.time + '> <span>' + time.time + '</span> ' + time.note
 
       ulListAlarm.append(li);
     })
 
+    if(alarmInputs === '') {
+      listInit()
+    }
+  }
+
+  // pushing of alarm in list of actives
+  let addAlarmActive = (check, value) => {
+    if(check) {
+      alarmListActivate.push(value);
+    } else {
+      let del = alarmListActivate.indexOf(value);
+      alarmListActivate.splice(del, 1);
+    }
+  }
+
+
+  let listInit = () => {
+    alarmInputs = document.getElementsByClassName('alarmInputs')
+
+    for(ai of alarmInputs) {
+      ai.addEventListener('click', (e) => {
+        addAlarmActive(e.target.checked, e.target.value)
+      })
+    }
+  }
+
+  // alarm
+  let alarmNow = () => {
+    console.log('OINK OINK OINK OINK!!!');
   }
 
 
@@ -124,14 +154,28 @@
     seconds++
   }, 1000)
 
+  let alarmClock = setInterval(() => {
+    console.log(alarmListActivate)
+    if(alarmListActivate.length > 0) {
+      let checker = (mHours + ':' + minutes + ampm);
+      console.log(alarmListActivate + '  ' + checker);
+      let testAlarm = alarmListActivate.filter((active) => {
+        return active == checker
+      })
+      if (testAlarm.length > 0) {
+        
+      }
+    }
+  }, 1000)
+
   inputHours.addEventListener('keypress', validateInput)
   inputMinutes.addEventListener('keypress', validateInput)
 
   let alarmList = []
+  let alarmListActivate = [];
   let alarmObj = {
     time: '',
     note: '',
-    alarm: false,
   }
 
   btnAddAlarm.addEventListener('click', addAlarm)
